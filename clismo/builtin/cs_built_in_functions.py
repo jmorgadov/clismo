16b9f4d4e68def8e55bc8e079afc1d40abc20e6c
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-02-19 00:26:56 -0500

    Fix builtin functions

--- clismo/builtin/cs_built_in_functions.py
+++ clismo/builtin/cs_built_in_functions.py
@@ -4,7 +4,7 @@ import random
 from time import sleep
 
 import clismo.exceptions as excpt
-from clismo.lang.type import Type, Instance
+from clismo.lang.type import Instance, Type
 
 __BUILTINS = {}
 
@@ -28,21 +28,21 @@ def resolve(func_name):
     return __BUILTINS.get(func_name, None)
 
 
-#region Numeric Functions
+# region Numeric Functions
 
 
 @builtin_func("abs", cs_float)
 def cs_abs(x: cs_float):
     if x.type.subtype(cs_float):
         return cs_float(builtins.abs(x.value))
-    # raise
+    raise TypeError("abs() argument must be a float")
 
 
 @builtin_func("bin", cs_str)
 def cs_bin(x: cs_int):
     if x.type.subtype(cs_int):
         return cs_str(builtins.bin(x.value))
-    # raise
+    raise TypeError("bin() argument must be an int")
 
 
 @builtin_func("round", cs_float)
@@ -52,8 +52,8 @@ def cs_round(x, ndigits=None):
             return cs_float(builtins.round(x.value))
         if ndigits.type.subtype(cs_int):
             return cs_float(builtins.round(x.value, ndigits.value))
-        # raise
-    # raise
+        raise TypeError("round() second argument must be an int")
+    raise TypeError("round() argument must be a float")
 
 
 @builtin_func("rand", cs_float)
@@ -63,9 +63,9 @@ def cs_rand():
 
 @builtin_func("randint", cs_int)
 def cs_randint(a, b):
-    if a.type.subtype(cs_float) and b.type.subtype(cs_int):
+    if a.type.subtype(cs_int) and b.type.subtype(cs_int):
         return cs_int(random.randint(a.value, b.value))
-    # raise
+    raise TypeError("randint() arguments must be int")
 
 
 @builtin_func("norm", cs_float)
@@ -77,14 +77,7 @@ def cs_norm():
 def cs_sqrt(x):
     if x.type.subtype(cs_float):
         return cs_float(math.sqrt(x.value))
-    # raise
-
-
-@builtin_func("sleep", cs_float)
-def cs_sleep(x: cs_float):
-    if x.type.subtype(cs_float):
-        return cs_float(sleep(x))
-    # raise
+    raise TypeError("sqrt() argument must be a float")
 
 
 @builtin_func("log", cs_float)
@@ -92,70 +85,70 @@ def log(x, base):
     if x.type.subtype(cs_float):
         if base.type.subtype(cs_int):
             return cs_float(math.log(x.value, base.value))
-        # raise
-    # raise
+        raise TypeError("log() second argument must be an int")
+    raise TypeError("log() argument must be a float")
 
 
 @builtin_func("log2", cs_float)
 def log2(x):
     if x.type.subtype(cs_float):
         return cs_float(math.log2(x.value))
-    # raise
+    raise TypeError("log2() argument must be a float")
 
 
 @builtin_func("exp", cs_float)
 def exp(x):
     if x.type.subtype(cs_float):
         return cs_float(math.exp(x.value))
-    # raise
+    raise TypeError("exp() argument must be a float")
 
 
 @builtin_func("ceil", cs_int)
 def ceil(x):
     if x.type.subtype(cs_int):
         return cs_int(math.ceil(x.value))
-    # raise
+    raise TypeError("ceil() argument must be an int")
 
 
 @builtin_func("floor", cs_int)
 def floor(x):
     if x.type.subtype(cs_int):
         return cs_int(math.floor(x.value))
-    # raise
+    raise TypeError("floor() argument must be an int")
 
 
 @builtin_func("sin", cs_float)
 def sin(x):
     if x.type.subtype(cs_float):
         return cs_float(math.sin(x.value))
-    # raise
+    raise TypeError("sin() argument must be a float")
 
 
 @builtin_func("cos", cs_float)
 def cos(x):
     if x.type.subtype(cs_float):
         return cs_float(math.cos(x.value))
-    # raise
+    raise TypeError("cos() argument must be a float")
 
 
 @builtin_func("tan", cs_float)
 def tan(x):
     if x.type.subtype(cs_float):
         return cs_float(math.tan(x.value))
-    # raise
+    raise TypeError("tan() argument must be a float")
 
 
-#endregion
+# endregion
 
 
-#region List Functions
+# region List Functions
 
 
 @builtin_func("len", cs_int)
 def cs_len(x):
     if x.type.subtype(cs_list):
-        return len(x.value)
-    # raise
+        return cs_int(len(x.value))
+    raise TypeError("len() argument must be a list")
 
 
 @builtin_func("get_at", lambda x, i: Type.get(x.type_name[:-5]))
@@ -163,24 +156,26 @@ def cs_get_at(x, index):
     if x.type.subtype(cs_list):
         if index.type.subtype(cs_int):
             return x.value[index.value]
-        # raise
-    # raise
+        raise TypeError("get_at() second argument must be an int")
+    raise TypeError("get_at() argument must be a list")
 
 
 @builtin_func("set_at", cs_none)
 def cs_set_at(x, index, obj):
     if x.type.subtype(cs_list):
         if index.type.subtype(cs_int):
-            x.value[index.value] = obj.value
-        # raise
-    # raise
+            x.value[index.value] = obj
+            return cs_none()
+        raise TypeError("set_at() second argument must be an int")
+    raise TypeError("set_at() argument must be a list")
+
 
 @builtin_func("append", lambda x, o: x)
 def cs_append(x, obj):
     if x.type.subtype(cs_list):
-        x.value.append(obj.value)
+        x.value.append(obj)
         return x
-    # raise
+    raise TypeError("append() argument must be a list")
 
 
 @builtin_func("list", lambda x: Type.get(f"{x}_list"))
@@ -188,38 +183,39 @@ def cs_new_list(type_):
     list_type = Type.get(f"{type_.value}_list")
     return Instance(list_type, [])
 
+
 # endregion
 
 
-#region String Functions
+# region String Functions
 
 
 @builtin_func("startswith", cs_bool)
-def cs_startswith(x):
-    if x.type.subtype(cs_str):
-        return str.startswith(x.value)
-    #raise
+def cs_startswith(x, y):
+    if x.type.subtype(cs_str) and y.type.subtype(cs_str):
+        return x.value.startswith(y.value)
+    raise TypeError("startswith() arguments must be strings")
 
 
 @builtin_func("isdigit", cs_bool)
 def cs_isdigit(x):
     if x.type.subtype(cs_str):
-        return str.isdigit(x.value)
-    #raise
+        return x.value.isdigit(x.value)
+    raise TypeError("isdigit() argument must be a str")
 
 
-builtin_func("lower", cs_str)
+@builtin_func("lower", cs_str)
 def cs_lower(x):
     if x.type.subtype(cs_str):
-        return str.lower(x.value)
-    #raise
+        return x.value.lower(x.value)
+    raise TypeError("lower() argument must be a str")
 
 
-builtin_func("capitalize", cs_str)
+@builtin_func("capitalize", cs_str)
 def cs_capitalize(x):
     if x.type.subtype(cs_str):
-        return str.capitalize(x.value)
-    #raise
+        return x.value.capitalize(x.value)
+    raise TypeError("capitalize() argument must be a str")
 
 
-#endregion
+# endregion
